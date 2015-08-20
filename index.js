8398bd2a86aec9dff833f2d9c00a95d284886b8c
Nick Sergeant <nick@nicksergeant.com>
2015-08-20 00:45:38 -0400

    Buttons.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var exec = require('child_process').exec;
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var sys = require('sys');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,6 +15,11 @@ app.post('/',function(req, res) {
   if (req.body.key !== process.env.KEY) return res.end('');
   console.log(req.body.button + ' pressed.');
   res.end(req.body.button);
+  switch (req.body.button) {
+    case 'other-button':
+      exec(process.env.OTHER_BUTTON);
+    break;
+  }
 });
 
 app.listen(process.env.PORT);
